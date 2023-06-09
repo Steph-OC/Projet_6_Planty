bc998aec1f94116dd7f980ed92f047c5b47c9ecb
Stephanie Quibel <nanou1972@msn.com>
2023-06-09 21:25:11 +0200

    Mise en place du footer

--- wp-content/themes/planty-child/footer.php
+++ wp-content/themes/planty-child/footer.php
@@ -1,1 +1,35 @@
-<?php wp_footer(); ?>
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Planty
+ */
+
+?>
+<footer id="colophon" class="site-footer">
+    <div class="site-info">
+
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'footer-menu',
+                'menu_id'       => 'menu_footer',
+            )
+        );
+        ?>
+
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer();
+
+?>
+</body>
+
+</html>
--- wp-content/themes/planty-child/functions.php
+++ wp-content/themes/planty-child/functions.php
@@ -1,12 +1,24 @@
 <?php
+
 /**
-** activation theme
-**/
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+ ** activation theme
+ **/
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
-function theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
- wp_enqueue_style('theme-style', get_stylesheet_directory_uri().'/assets/css/theme.css', array(),
- filemtime(get_stylesheet_directory().'/assets/css/theme.css'));
-
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style(
+        'theme-style',
+        get_stylesheet_directory_uri() . '/assets/css/theme.css',
+        array(),
+        filemtime(get_stylesheet_directory() . '/assets/css/theme.css')
+    );
 }
+// This theme uses wp_nav_menu() in one location and footer menu
+register_nav_menus(
+    array(
+        'menu-1' => esc_html__('Primary', 'planty'),
+        'footer-menu' => esc_html__('Footer-menu', 'planty')
+    )
+);
