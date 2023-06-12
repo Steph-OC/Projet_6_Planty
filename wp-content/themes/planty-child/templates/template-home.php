dff90449e09723f6167637d68b9b48b159bd2523
Stephanie Quibel <nanou1972@msn.com>
2023-06-12 14:41:07 +0200

    création template-home.php

---
+++ wp-content/themes/planty-child/templates/template-home.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Template Name: Home Template
+ */
+/*
+ get_header(); ?>
+
+<main id="primary" class="site-main">
+   
+
+<?php planty_post_thumbnail(); ?>
+
+</main>
+<?php
+
+ get_footer(); ?>
+
+
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Planty
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+    <section class="pl-banner">
+        <h1>BOISSON ENERGISANTE 100% NATURELLE</h1>
+        <img class="img-banner-1" src="<?= get_stylesheet_directory_uri() . "/assets/img/Group 17.png" ?>">
+    </section>
+
+</main><!-- #main -->
+
+<?php
+
+get_footer();
