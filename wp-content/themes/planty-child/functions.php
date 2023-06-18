2107e1661558e37b1f55a678175a887d26d87328
Stephanie Quibel <nanou1972@msn.com>
2023-06-18 15:13:52 +0200

    mise en place du template page avec spectra

--- wp-content/themes/planty-child/functions.php
+++ wp-content/themes/planty-child/functions.php
@@ -8,11 +8,8 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style(
-        'theme-style',
-        get_stylesheet_directory_uri() . '/assets/css/theme.css',
-        array(),
-        filemtime(get_stylesheet_directory() . '/assets/css/theme.css')
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css',
+    array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css')
     );
 }
 // This theme uses wp_nav_menu() in one location and footer menu
@@ -22,3 +19,5 @@ register_nav_menus(
         'footer-menu' => esc_html__('Footer-menu', 'planty')
     )
 );
+
+
