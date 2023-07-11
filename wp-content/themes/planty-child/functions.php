7ce7a06160dedaf5d676bdb44c572032993e6a17
Stephanie Quibel <nanou1972@msn.com>
2023-07-11 16:31:32 +0200

    récupération formulaire spinbox

--- wp-content/themes/planty-child/functions.php
+++ wp-content/themes/planty-child/functions.php
@@ -1,15 +1,20 @@
 <?php
+
 /**
  ** activation theme
  **/
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+// delete <p> to contact form 7
+add_filter('wpcf7_autop_or_not', '__return_false');
 
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('theme-style',  get_stylesheet_directory_uri() . '/assets/css/theme.css',
-        array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css')
+    wp_enqueue_style(
+        'theme-style',
+        get_stylesheet_directory_uri() . '/assets/css/theme.css',
+        array(),
+        filemtime(get_stylesheet_directory() . '/assets/css/theme.css')
     );
 }
 // This theme uses wp_nav_menu() in one location and footer menu
@@ -19,9 +24,3 @@ register_nav_menus(
         'footer-menu' => esc_html__('Footer-menu', 'planty')
     )
 );
-
-// Remove auto p from Contact Form 7 
-add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
-function wpcf7_autop_return_false() {
-    return false;
-} 
